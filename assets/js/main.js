9cd04e80a667f8c2c9231b7c817062ff0676aef7
Victor <victorsantos@ufmg.br>
2020-06-19 18:25:11 -0300

    add sticky header

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// STICKY HEADER
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickHeader()};
+
+// Get the header
+var header = document.getElementById("page-header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
